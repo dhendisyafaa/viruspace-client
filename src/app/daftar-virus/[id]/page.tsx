89449bb2781b2c8fe8ed90e4f10875d646558549
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2024-12-02 10:56:21 +0700

    feat: add detail virus page

---
+++ src/app/daftar-virus/[id]/page.tsx
@@ -0,0 +1,71 @@
+import DetailVirusImage from "@/components/DetailVirusImage";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import dataDetailVirus from "../../../constans/detail-virus.json";
+
+export default async function DetailVirus({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const virusById = dataDetailVirus.find((virus) => virus.id === Number(id));
+
+  if (!virusById) {
+    return (
+      <div className="container mx-auto text-center py-10">
+        <h1 className="text-3xl font-bold text-red-500">
+          Virus tidak ditemukan
+        </h1>
+        <p>Periksa URL Anda dan coba lagi.</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container p-4 mx-auto gap-5 grid grid-cols-1 md:grid-cols-2">
+      <DetailVirusImage images={virusById?.images} />
+      <div className="pb-10">
+        <div className="p-4 w-full sticky top-0 bg-[#fff8f3]">
+          <p className="text-primary uppercase text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">
+            {virusById?.name}
+          </p>
+          <p className="text-primary uppercase text-xs sm:text-sm md:text-lg lg:text-xl font-medium">
+            {virusById?.penyebab}
+          </p>
+        </div>
+        {/* If you want to write it to the DOM, pass a string instead: collapsible="true" or collapsible={value.toString()} */}
+        <div className="space-y-5 bg-primary text-primary-foreground p-4 rounded-3xl">
+          <Accordion type="multiple" defaultValue={["item-1"]}>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>PENGERTIAN</AccordionTrigger>
+              <AccordionContent>{virusById?.pengertian}</AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+              <AccordionTrigger>BENTUK</AccordionTrigger>
+              <AccordionContent>{virusById?.bentuk}</AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+              <AccordionTrigger>PENYEBARAN DAN GEJALA AWAL</AccordionTrigger>
+              <AccordionContent>
+                {virusById?.penyebaranGejalaAwal}
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-4">
+              <AccordionTrigger>BIASA DITEMUKAN</AccordionTrigger>
+              <AccordionContent>{virusById?.tempatDitemukan}</AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-5">
+              <AccordionTrigger>CARA MENGATASI/MENCEGAH</AccordionTrigger>
+              <AccordionContent>{virusById?.caraPencegahan}</AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/DetailVirusImage.tsx
@@ -0,0 +1,111 @@
+"use client";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import Autoplay from "embla-carousel-autoplay";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function DetailVirusImage({ images }) {
+  const [openModal, setOpenModal] = useState(false);
+  const autoplayDuration = 5000;
+  return (
+    <div className="w-full mx-auto">
+      <div className="hidden md:block w-full sticky top-4 columns-2 gap-4 space-y-4">
+        {images.map((image, idx) => {
+          return (
+            <div
+              key={idx}
+              onClick={() => setOpenModal(true)}
+              className="cursor-pointer"
+            >
+              <Image
+                className="rounded-xl shadow w-full h-auto"
+                src={image}
+                width={0}
+                height={0}
+                sizes="100vw"
+                alt={`image virus ${idx + 1}`}
+              />
+            </div>
+          );
+        })}
+      </div>
+      <Carousel
+        className="w-full md:hidden"
+        plugins={[
+          Autoplay({
+            delay: autoplayDuration,
+          }),
+        ]}
+        opts={{
+          loop: true,
+        }}
+      >
+        <CarouselContent>
+          {images.map((image, idx) => {
+            return (
+              <CarouselItem key={idx}>
+                <div onClick={() => setOpenModal(true)}>
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6 cursor-pointer">
+                      <img
+                        className="w-full rounded-xl shadow"
+                        src={image}
+                        width="232"
+                        height="290"
+                        alt={`image virus ${idx + 1}`}
+                      />
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+      <Dialog open={openModal} onOpenChange={setOpenModal}>
+        <DialogContent>
+          <Carousel
+            plugins={[
+              Autoplay({
+                delay: autoplayDuration,
+              }),
+            ]}
+            opts={{
+              loop: true,
+            }}
+          >
+            <CarouselContent>
+              {images.map((image, idx) => {
+                return (
+                  <CarouselItem key={idx}>
+                    <div className="relative w-full aspect-square mx-auto cursor-grab">
+                      <Image
+                        className="border"
+                        src={image}
+                        alt={`image virus ${idx + 1}`}
+                        layout="fill"
+                        objectFit="contain"
+                      />
+                    </div>
+                  </CarouselItem>
+                );
+              })}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
