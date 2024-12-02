691205cd5a596b648cd40c184acc43187cfd5534
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2024-12-02 10:58:08 +0700

    feat: list of viruses page

--- src/app/daftar-virus/page.tsx
+++ src/app/daftar-virus/page.tsx
@@ -1,60 +1,50 @@
 import Image from "next/image";
+import listViruses from "../../constans/daftar-virus.json";
+import Link from "next/link";
 
 export default function ListOfViruses() {
-  const listViruses = [
-    {
-      id: 1,
-      name: "Corona",
-      image: "/images/rabies.png",
-      bgColor: "#13525E",
-    },
-    {
-      id: 2,
-      name: "rabies",
-      image: "/images/rabies.png",
-      bgColor: "#46762C",
-    },
-    {
-      id: 3,
-      name: "Polio",
-      image: "/images/rabies.png",
-      bgColor: "#847A0F",
-    },
-    {
-      id: 4,
-      name: "hiv",
-      image: "/images/rabies.png",
-      bgColor: "#7D356D",
-    },
-    {
-      id: 5,
-      name: "influenza",
-      image: "/images/rabies.png",
-      bgColor: "#542718",
-    },
-  ];
   return (
-    <div className="container w-[90vw] mx-auto grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-2 sm:gap-6 lg:gap-8">
-      {listViruses.map((virus) => {
-        return (
-          <div
-            key={virus.id}
-            className="relative aspect-square rounded-3xl overflow-hidden"
-            style={{ backgroundColor: virus.bgColor }}
-          >
-            <Image
-              src={virus.image}
-              alt={virus.name}
-              layout="fill"
-              objectFit="contain"
-              className="border border-red-900"
-            />
-            <p className="mt-10 text-4xl uppercase text-white font-bold text-center">
-              {virus.name}
-            </p>
-          </div>
-        );
-      })}
+    <div className="w-[90vw] pb-10 mx-auto">
+      <div className="text-center pb-10 space-y-3">
+        <p className="text-xl md:text-3xl font-bold text-primary uppercase">
+          Pelajari virus-virus di sekitar kita
+        </p>
+        <p className="text-base mx-auto md:text-xl md:w-[60vw] text-primary">
+          Lihat{" "}
+          <span className="font-bold">
+            penjelasan lengkap, cara pencegahan, dan penyebaran
+          </span>{" "}
+          tentang virus di sekitar
+        </p>
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-2 sm:gap-6 lg:gap-8">
+        {listViruses.map((virus) => {
+          return (
+            // http://localhost:3000/daftar-virus/2?name=sars-cov-2
+            <Link
+              href={`/daftar-virus/${virus.id}?name=${virus.name}`}
+              key={virus.id}
+            >
+              <div
+                className="relative aspect-square rounded-3xl overflow-hidden"
+                style={{ backgroundColor: virus.bgColor }}
+              >
+                <p className="relative top-5 md:top-8 z-40 text-xs sm:text-lg md:text-xl lg:text-2xl uppercase text-white font-bold text-center">
+                  {virus.name}
+                </p>
+                <Image
+                  src={virus.image}
+                  alt={virus.name}
+                  width="0"
+                  height="0"
+                  sizes="100vw"
+                  className="w-full h-fit bottom-3 absolute"
+                />
+              </div>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
