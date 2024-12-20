d7c33e13d6cfe4c66288ee51532ebe78ab331c5b
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2024-12-20 20:03:54 +0700

    feat: split component for list of viruses

--- src/app/daftar-virus/page.tsx
+++ src/app/daftar-virus/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
-import listViruses from "../../constans/daftar-virus.json";
-import Link from "next/link";
+import ListOfViruses from "@/components/ListOfViruses";
 
-export default function ListOfViruses() {
+export default function ListOfVirusesPage() {
   return (
     <div className="w-[90vw] pb-10 mx-auto">
       <div className="text-center pb-10 space-y-3">
@@ -19,33 +17,7 @@ export default function ListOfViruses() {
           dilindungi oleh lapisan protein (kapsid)
         </p>
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-2 sm:gap-6 lg:gap-8">
-        {listViruses.map((virus) => {
-          return (
-            <Link
-              href={`/daftar-virus/${virus.id}?name=${virus.name}`}
-              key={virus.id}
-            >
-              <div
-                className="relative aspect-square rounded-3xl overflow-hidden"
-                style={{ backgroundColor: virus.bgColor }}
-              >
-                <p className="relative top-5 md:top-8 z-40 text-xs sm:text-lg md:text-xl lg:text-2xl uppercase text-white font-bold text-center">
-                  {virus.name}
-                </p>
-                <Image
-                  src={virus.image}
-                  alt={virus.name}
-                  width="0"
-                  height="0"
-                  sizes="100vw"
-                  className="w-full h-fit bottom-3 absolute"
-                />
-              </div>
-            </Link>
-          );
-        })}
-      </div>
+      <ListOfViruses />
     </div>
   );
 }
